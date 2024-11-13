4b4d18dfbdd2206d4c8a1692e9cc134ef7f23796
edoj2k <edwardferdian@yahoo.com>
2024-11-13 13:01:28 +0700

    closure

--- index.php
+++ index.php
@@ -1,34 +1,24 @@
 <?php
-
-// $create_printer = function () {
-//     return function () {
-//         echo "Hello functional!\n";
+// $create_function = function () {
+//     $x = 5;
+//     return function () use ($x) {
+//         echo "x is $x \n";
 //     };
 // };
 
-$create_printer = fn() => fn() => "Hello functional!\n";
+$create_function = fn($x = 5) => fn() => "x is $x \n";
+
+$function = $create_function();
+echo $function();
+
+// $create_printer = function () {
+//     $my_favorite_number = 42;
+//     return function () use ($my_favorite_number) {
+//         echo "My favorite number is $my_favorite_number \n";
+//     };
+// };
+
+$create_printer = fn($my_favorite_number = 42) => fn() => "My favorite number is $my_favorite_number \n";
 
 $my_printer = $create_printer();
 echo $my_printer();
-
-// $double = fn($x) => $x * 2;
-// $triple = fn($x) => $x * 3;
-// $quadruple = fn($x) => $x * 4;
-
-// $create_multiplier = function ($y) {
-//     return function ($x) use ($y) {
-//         return $x * $y;
-//     };
-// };
-
-$create_multiplier = fn($y) => fn($x) => $x * $y;
-
-$double = $create_multiplier(2);
-$triple = $create_multiplier(3);
-$quadruple = $create_multiplier(4);
-
-echo $create_multiplier(2)(3) . "\n";
-
-echo $double(3) . "\n";
-echo $triple(3) . "\n";
-echo $quadruple(3) . "\n";
