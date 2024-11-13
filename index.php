2e8643760258e52f3341c349ccb62427de6fbc72
edoj2k <edwardferdian@yahoo.com>
2024-11-13 09:36:35 +0700

    change name

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
 
-echo 'Hello';
+use FFI\ParserException;
+
+class Person
+{
+    public $name, $age;
+
+    function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    function increase_age()
+    {
+        $this->age += 2;
+    }
+}
+
+$person = [
+    'name' => 'naufal',
+    'age' => 17
+];
+
+function change_name($person, $new_name)
+{
+    return [
+        'name' => $new_name,
+        'age' => $person['age']
+    ];
+}
+
+$test = change_name($person, 'rafi');
+
+echo $test['name'];
