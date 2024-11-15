0520d1f045ce84d31f96f0ceee69298f71b3b41d
edoj2k <edwardferdian@yahoo.com>
2024-11-15 13:34:00 +0700

    combining array functions

--- index.php
+++ index.php
@@ -1,12 +1,48 @@
 <?php
 
-$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$employees = [
+    [
+        'name' => 'Naufal',
+        'age' => 18,
+        'salary' => 90000,
+        'job_title' => 'developer'
+    ],
+    [
+        'name' => 'Rafi',
+        'age' => 16,
+        'salary' => 110000,
+        'job_title' => 'developer'
+    ],
+    [
+        'name' => 'Sulthan',
+        'age' => 13,
+        'salary' => 35000,
+        'job_title' => 'construction worker'
+    ],
+    [
+        'name' => 'Rajwa',
+        'age' => 11,
+        'salary' => 55000,
+        'job_title' => 'digital marketing specialist'
+    ]
+];
 
-$get_sum = function ($carry, $item) {
-    echo '$carry : ' . $carry . ' , $item : ' . $item . "\n";
-    return $carry + $item;
-};
+$developers = array_filter($employees, fn($x) => $x['job_title'] == 'developer');
+$developer_salaries = array_map(fn($x) => $x['salary'], $developers);
+$total_developer_salaries = array_reduce($developer_salaries, fn($carry, $item) => $carry + $item);
+$average_developer_salary = $total_developer_salaries / count($developer_salaries);
 
-// $get_sum = array_reduce($numbers, fn($carry, $item) => $carry + $item);
-$sum = array_reduce($numbers, $get_sum, 0);
-echo ($sum) . "\n";
+$non_developers = array_filter($employees, fn($x) => $x['job_title'] != 'developer');
+$non_developer_salaries = array_map(fn($x) => $x['salary'], $non_developers);
+$total_non_developer_salaries = array_reduce($non_developer_salaries, fn($carry, $item) => $carry + $item);
+$average_non_developer_salary = $total_non_developer_salaries / count($non_developer_salaries);
+
+print_r($developers);
+print_r($developer_salaries);
+echo ($total_developer_salaries) . "\n";
+echo "The average developer salary is $average_developer_salary \n";
+
+print_r($non_developers);
+print_r($non_developer_salaries);
+echo ($total_non_developer_salaries) . "\n";
+echo "The average non developer salary is $average_non_developer_salary \n";
