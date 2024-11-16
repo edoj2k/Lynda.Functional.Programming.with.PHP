b062193abc6639990b446b594cfc9abf61d9837c
edoj2k <edwardferdian@yahoo.com>
2024-11-16 15:20:32 +0700

    composition

--- index.php
+++ index.php
@@ -1,25 +1,67 @@
 <?php
 
-function count_down($x)
-{
-    if ($x < 0) {
-        echo "Blast off!\n";
-        return;
-    }
-    echo $x . "...\n";
-    count_down($x - 1);
-}
+$people_data = [
+    [
+        'full_name' => 'Ed Jhonson',
+        'age' => 28,
+        'height' => 72
+    ],
+    [
+        'full_name' => 'Jeff Ruiz',
+        'age' => 35,
+        'height' => 65
+    ],
+    [
+        'full_name' => 'Jane Price',
+        'age' => 43,
+        'height' => 60
+    ]
+];
 
-count_down(10);
+$with_first_and_last_name = function ($person) {
+    return array_merge(
+        $person,
+        [
+            'first_name' => explode(' ', $person['full_name'])[0],
+            'last_name' => explode(' ', $person['full_name'])[1]
+        ]
+    );
+};
 
-function count_up($x, $max)
-{
-    if ($x > $max) {
-        echo "Done.\n";
-        return;
-    }
-    echo $x . "...\n";
-    count_up($x + 1, $max);
-}
+$height_inches_to_meters = function ($person) {
+    return array_merge(
+        $person,
+        [
+            'height' => $person['height'] * 0.0254,
+        ]
+    );
+};
 
-count_up(0, 10);
+$add_initials = function ($person) {
+    return array_merge(
+        $person,
+        [
+            'initials' => $person['first_name'][0] . $person['last_name'][0]
+        ],
+    );
+};
+
+$compose = function (...$funcs) {
+    return function ($data) use ($funcs) {
+        return array_reduce(
+            $funcs,
+            fn($carry, $func) => $func($carry),
+            $data,
+        );
+    };
+};
+
+$format_person = $compose(
+    $with_first_and_last_name,
+    $height_inches_to_meters,
+    $add_initials
+);
+
+$formatted_people = array_map($format_person, $people_data);
+
+print_r($formatted_people);
